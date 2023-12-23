0b68486716396520ca38909b0f290411261a0d27
georgychomakhashvili <ge95kan@mytum.de>
2023-12-23 17:05:56 +0100

    Initial Commit

---
+++ Pocket Ledger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket Ledger
+//
+//  Created by Georgy Chomakhashvili on 23.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pocket Ledger/Pocket_LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_LedgerApp.swift
+//  Pocket Ledger
+//
+//  Created by Georgy Chomakhashvili on 23.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
